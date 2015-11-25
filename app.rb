2a46cae8e53b5fbbd27c4a85de13e93e01949ee0
Ivan Itzcovich <i.itzcovich@gmail.com>
2015-11-25 13:24:24 -0300

    mRNA

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ end
 class MainHTML
 
   def self.getLayout
-    return "<html><head><title>Bio-ITBA</title><link href='https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css' rel='stylesheet' type='text/css' /></head><body style='margin-left:50px'><div style='width:100%, margin-left:50px'><h1 class='text-info'>Species Blastp By Hanna, Itzcovich, Kraus</h1><br><form method=post><div><textarea rows='20' cols='100' name=fasta style='width:50%'></textarea></div><button type='button submit' class='btn btn-primary'>Run</button></form></html></div></body>"
+    return "<html><head><title>Bio-ITBA</title><link href='https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css' rel='stylesheet' type='text/css' /></head><body style='margin-left:50px'><div style='width:100%, margin-left:50px'><h1 class='text-info'>Species Blastp By Hanna, Itzcovich, Kraus</h1><br><form method=post><div><textarea rows='20' cols='100' name=fasta style='width:50%'></textarea></div><input type='radio' name='type' value='fasta' checked>Protein Fasta<br><input type='radio' name='type' value='gen'>GenBank mRNA<br><br><button type='button submit' class='btn btn-primary'>Run</button></form></html></div></body>"
   end
 
 end
@@ -63,19 +63,39 @@ Cuba.define do
 
     blast = Bio::Blast.remote('blastp', 'swissprot', '-e 0.0001', 'genomenet')
 
-    fasta = Bio::FastaFormat.new( req.POST["fasta"])
+    if req.POST["type"]=="fasta"
+      fasta = Bio::FastaFormat.new(req.POST["fasta"])
+      puts fasta
+    elsif req.POST["type"]=="gen"
+
+      genbank_content = Bio::GenBank.new(req.POST["fasta"])
+
+      fasta = Bio::FastaFormat.new(Bio::Sequence::NA.new(genbank_content.to_biosequence).translate.to_fasta("protein"))
+      puts fasta
+
+    end
+      
 
     results = []
 
     report = blast.query(fasta.entry)
+
     report.hits.each_with_index do |hit, i|
-      break if i >= 10
+      break if i >= 15
 
       accessor = hit.definition.split(" ")[0]
       number_accessor = accessor[1..-2]
       str = open("http://www.uniprot.org/uniprot/#{number_accessor}.txt").read
 
-      results << [Bio::UniProtKB.new(str).os.first["os"], hit.hsps.first.score, hit.identity, hit.len, hit.overlap, hit.query_seq, hit.target_seq, number_accessor]
+      species = Bio::UniProtKB.new(str).os.first["os"]
+
+      flag = false;
+
+      results.each { |x| flag = true if x[0] == species  }
+
+      if !flag 
+        results << [species, hit.hsps.first.score, hit.identity, hit.len, hit.overlap, hit.query_seq, hit.target_seq, number_accessor]
+      end
     end
 
     id =  Memo.set(results)
